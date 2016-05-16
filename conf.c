85aee6692ea18c5f3e02a3225f73995b2d1511f2
simplex1337 <cas98@bk.ru>
2016-05-16 12:53:13 +0600

    conflict done(done)

--- conf.c
+++ conf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main() {
     printf("Hello, World\n");
-<<<<<<< HEAD
     printf("Mad, World\n");
-=======
     printf("Goodbye, World\n");
->>>>>>> vetka
+
     return 0;
 }
